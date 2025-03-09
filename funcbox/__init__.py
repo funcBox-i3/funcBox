a6dc96bde15d5f76e4dd8d1d4987de72f34b6302
Aditya Prasad S <70315903+adityaprasad502@users.noreply.github.com>
2025-03-09 13:22:42 +0530

    new commit - 0.2.0

--- funcbox/__init__.py
+++ funcbox/__init__.py
@@ -1,7 +1,6 @@
-from .functions import is_even, is_odd, factorial, is_prime, is_palindrome, is_armstrong
+from .functions import *
 
 # Aliases
 fact = factorial
 
-
-__version__ = "0.1.1"
+__version__ = "0.2.0"
